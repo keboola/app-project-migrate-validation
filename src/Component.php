6fe283d7aa330cca063740eedb70e64e3b9a9a09
Martin Halamíček <martin@halamicek.cz>
2018-05-24 17:55:19 +0200

    component run validation

--- src/Component.php
+++ src/Component.php
@@ -5,12 +5,36 @@ declare(strict_types=1);
 namespace Keboola\ProjectMigrateValidation;
 
 use Keboola\Component\BaseComponent;
+use Keboola\Component\UserException;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Components;
 
 class Component extends BaseComponent
 {
     public function run(): void
     {
-        // @TODO implement
+        $storageClient = new Client([
+            'url' => getenv('KBC_URL'),
+            'token' => getenv('KBC_TOKEN'),
+            'runId' => getenv('KBC_RUNID'),
+        ]);
+
+        $validate = new Validate(
+            new Components($storageClient),
+            GoodDataWriterClientV2::createFromStorageClient($storageClient)
+        );
+
+        $results = $validate->run();
+
+        if (empty($results)) {
+            $this->getLogger()->info('✔ Project is valid and can be migrated.');
+            return;
+        }
+
+        foreach ($results as $resultMessage) {
+            $this->getLogger()->error($resultMessage);
+        }
+        throw new UserException('Project cannot be migrated, please resolve first validation issues');
     }
 
     protected function getConfigClass(): string
--- src/GoodDataWriterClientV2.php
+++ src/GoodDataWriterClientV2.php
@@ -6,6 +6,8 @@ namespace Keboola\ProjectMigrateValidation;
 
 use Keboola\Writer\GoodData\Client;
 
+use Keboola\StorageApi\Client as StorageClient;
+
 class GoodDataWriterClientV2
 {
     /** @var Client  */
@@ -14,6 +16,32 @@ class GoodDataWriterClientV2
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    public static function getKeboolaServiceUrl(array $services, string $serviceId): string
+    {
+        $foundServices = array_values(array_filter($services, function ($service) use ($serviceId) {
+            return $service['id'] === $serviceId;
+        }));
+        if (empty($foundServices)) {
+            throw new \Exception(sprintf('%s service not found', $serviceId));
+        }
+        return $foundServices[0]['url'];
+    }
+
+    public static function createFromStorageClient(StorageClient $sapiClient): self
+    {
+        $services =  $sapiClient->indexAction()['services'];
+        $baseUrl = self::getKeboolaServiceUrl(
+            $services,
+            'syrup'
+        );
+        $goodDataWriterClient = Client::factory([
+            'url' => sprintf("%s/gooddata-writer", $baseUrl),
+            'token' => $sapiClient->getTokenString(),
+            'runId' => $sapiClient->getRunId(),
+        ]);
+        return new self($goodDataWriterClient);
     }
 
     public function getWriters(): array
