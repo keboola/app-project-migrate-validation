1c17d4b30797c38a70b25dec1dcf2ea17b64eb6f
Ondra Jodas <ondrej.jodas@keboola.com>
2022-06-30 08:33:31 +0200

    fix phpstan

--- src/Config.php
+++ src/Config.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Keboola\ProjectMigrateValidation;
 
 use Keboola\Component\Config\BaseConfig;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class Config extends BaseConfig
 {
@@ -15,6 +16,13 @@ class Config extends BaseConfig
             $url = 'https://' . $url;
         }
         $urlParts = parse_url($url);
+        foreach (['scheme', 'host'] as $item) {
+            if (empty($urlParts[$item])) {
+                throw new InvalidConfigurationException(
+                    sprintf('Missing "%s", in the destination URL address', $item)
+                );
+            }
+        }
         return sprintf('%s://%s/', $urlParts['scheme'], $urlParts['host']);
     }
 
--- tests/phpunit/ConfigTest.php
+++ tests/phpunit/ConfigTest.php
@@ -56,6 +56,4 @@ class ConfigTest extends TestCase
             ],
         ];
     }
-
-
 }
